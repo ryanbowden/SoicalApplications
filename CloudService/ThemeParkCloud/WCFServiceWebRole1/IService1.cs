c068859a891ba34b806dde1414b1f23a115af42b
Ryan Bowden <ryanbowden1@outlook.com>
2014-05-01 11:47:59 +0100

    Added Cloud Connection

--- CloudService/ThemeParkCloud/WCFServiceWebRole1/IService1.cs
+++ CloudService/ThemeParkCloud/WCFServiceWebRole1/IService1.cs
@@ -12,10 +12,32 @@ namespace WCFServiceWebRole1
     [ServiceContract]
     public interface IService1
     {
-
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+         ResponseFormat = WebMessageFormat.Json,
+         BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "viewthemeparks?format=json")]
+        ThemeParkList[] viewThemeParksJson();
     }
 
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ThemeParkList
+    {
+        [DataMember]
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
 
+        private string name;
+        public string Name
+        {
+            get { return name;}
+            set{ name = value;}
+        }
+    }
 }
--- CloudService/ThemeParkCloud/WCFServiceWebRole1/Service1.svc.cs
+++ CloudService/ThemeParkCloud/WCFServiceWebRole1/Service1.svc.cs
@@ -5,6 +5,10 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
 
 namespace WCFServiceWebRole1
 {
@@ -12,7 +16,43 @@ namespace WCFServiceWebRole1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+        public ThemeParkList[] viewThemeParksJson()
+        {
+            //Need to get list of Theme parks and return it in Json.
+            // get the connections string stored in the web.config file as a string
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            // create  new sql connections using the connection string
+            SqlConnection thisConnection = new SqlConnection(connectionString);
+            //Sql Command
+            SqlCommand getThemeParks = thisConnection.CreateCommand();
+            //Create a temp List to hold the data
+            List<ThemeParkList> themeparks = new List<ThemeParkList>();
+            //Open SQL Command
+            thisConnection.Open();
+            string sql = "SELECT * FROM ThemeParks";
 
+            getThemeParks.CommandText = sql;
+
+            //Create a SQL data adapter 
+            SqlDataAdapter da = new SqlDataAdapter(getThemeParks);
+            //Create a new Dataset 
+            DataSet ds = new DataSet();
+            da.Fill(ds, "ThemeParks");
+
+            //Now get every row and 
+
+            foreach (DataRow dr in ds.Tables["ThemeParks"].Rows)
+            {
+                themeparks.Add(new ThemeParkList()
+                {
+                    ID = Convert.ToInt32(dr[0]),
+                    Name = Convert.ToString(dr[1])
+                });
+            }
+
+            thisConnection.Close();
+            return themeparks.ToArray();
+        }
        
     }
 }
