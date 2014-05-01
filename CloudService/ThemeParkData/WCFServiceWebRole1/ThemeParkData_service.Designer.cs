26e9c401e2c42c4700fe9977d61bef3a7fa1e22f
Ryan Bowden <ryanbowden1@outlook.com>
2014-05-01 11:05:51 +0100

    Removed Cloud as it was done and named wrong.

--- CloudService/ThemeParkData/WCFServiceWebRole1/ThemeParkData_service.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Ryan\Documents\GitHub\SoicalApplications\CloudService\ThemeParkData\WCFServiceWebRole1\ThemeParkData_service.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
